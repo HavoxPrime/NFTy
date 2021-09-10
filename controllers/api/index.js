996ea9adb340b7ad4100110393654e03f42e1d84
Adrian <adrian.auch11@gmail.com>
2021-09-10 13:25:51 -0600

    fixed the formatting for the login page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const collectionRoutes = require("./collection-routes.js");
 const nftRoutes = require("./nft-routes");
 const userRoutes = require("./user-routes");
+const userWorksRoutes = require("./userworks-routes");
 
 router.use("/collection", collectionRoutes);
 router.use("/nft", nftRoutes);
 router.use("/user", userRoutes);
+router.use("/userworks", userWorksRoutes);
 
 module.exports = router;
